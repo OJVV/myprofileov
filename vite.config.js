0a8f9de1d3c71300a5e256d907a2a189e6de97e1
Orlando Vidaurre <126068352+OJVV@users.noreply.github.com>
2023-10-25 15:22:36 +0200

    vite resources change

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'public/assets/css/style.css',
+                'resources/scss/style.scss'
             ],
             refresh: [
                 ...refreshPaths,
